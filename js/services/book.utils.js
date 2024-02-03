2a4f10cfbb2831772b7479527b3a5a791f0ecac2
omribarouch <omri.barouch@safebreach.com>
2024-02-03 23:02:33 +0200

    Add makeId func

--- js/services/book.utils.js
+++ js/services/book.utils.js
@@ -1,1 +1,12 @@
 'use strict'
+
+function makeId(length) {
+    var id = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        id += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return id
+}
