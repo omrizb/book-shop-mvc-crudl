acb491e778aa2418f9d5f23b9095f61f81cceecf
omribarouch <omri.barouch@safebreach.com>
2024-02-04 23:30:58 +0200

    Render default data from model

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,5 +5,22 @@ function onInit() {
 }
 
 function render() {
-    console.log('Hi')
+    var htmlStr = ''
+    var books = getBooks()
+    books.forEach(book => {
+        htmlStr += `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td>
+                <button class="btn1 read-btn">Read</button>
+                <button class="btn1 update-btn">Update</button>
+                <button class="btn1 delete-btn">Delete</button>
+            </td>
+        </tr>
+        `
+    })
+
+    const elTable = document.querySelector('tbody.books-data')
+    elTable.innerHTML = htmlStr
 }
